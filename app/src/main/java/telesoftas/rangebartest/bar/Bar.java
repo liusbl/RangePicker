23f369d2f75343a558b8d28b09bab9ecc8caa499
Liudvikas <liusbl@telesoftas.com>
2017-10-19 11:33:49 +0300

    Initialize structure

---
+++ app/src/main/java/telesoftas/rangebartest/bar/Bar.java
@@ -0,0 +1,22 @@
+package telesoftas.rangebartest.bar;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.ColorInt;
+
+public class Bar {
+    private final Paint paint;
+    private final float length;
+
+    public Bar(int length, float width, @ColorInt int color) {
+        this.length = length;
+        paint = new Paint();
+        paint.setStrokeWidth(width);
+        paint.setColor(color);
+        paint.setAntiAlias(true);
+    }
+
+    void draw(Canvas canvas) {
+        canvas.drawLine(0, 0, length, 0, paint);
+    }
+}
