a062248f9ce6454f62a3242c7ae31fa8afb7ce34
Liudvikas <liusbl@telesoftas.com>
2017-10-19 13:24:20 +0300

    Merge RangeBar with Bar

--- app/src/main/java/telesoftas/rangebartest/bar/Bar.java
+++ app/src/main/java/telesoftas/rangebartest/bar/Bar.java
@@ -1,15 +1,38 @@
 package telesoftas.rangebartest.bar;
 
+import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.support.annotation.ColorInt;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
 
-public class Bar {
-    private final Paint paint;
-    private final float length;
+import telesoftas.rangebartest.R;
+import utils.TypedAttributes;
+import utils.TypedAttributesImpl;
 
-    public Bar(int length, float width, @ColorInt int color) {
-        this.length = length;
+public class Bar extends View {
+    private Paint paint;
+    private int length;
+
+    public Bar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public Bar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attributeSet) {
+        TypedAttributes attributes = new TypedAttributesImpl(context,
+                attributeSet, R.styleable.Bar);
+        length = attributes.getLayoutDimension(R.styleable.Bar_length);
+        int width = attributes.getLayoutDimension(R.styleable.Bar_width) * 2;
+        int color = attributes.getColor(R.styleable.Bar_color);
+        attributes.recycle();
         paint = createPaint(width, color);
     }
 
@@ -22,7 +45,9 @@ public class Bar {
         return paint;
     }
 
-    void draw(Canvas canvas) {
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
         canvas.drawLine(0, 0, length, 0, paint);
     }
 }
