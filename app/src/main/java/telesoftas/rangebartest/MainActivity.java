dc5e2ccb3c0da63e556e4d48855c8a8c52225340
Liudvikas <liusbl@telesoftas.com>
2017-11-21 15:58:44 +0200

    Cleanup classes

--- app/src/main/java/telesoftas/rangebartest/MainActivity.java
+++ app/src/main/java/telesoftas/rangebartest/MainActivity.java
@@ -7,26 +7,27 @@ import android.widget.TextView;
 import telesoftas.rangebartest.bar.RangePicker;
 
 public class MainActivity extends AppCompatActivity implements RangePicker.OnRangeChangeListener {
-    RangePicker bars;
-    TextView start;
-    TextView end;
+    private TextView start;
+    private TextView end;
 
-    @Override protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bars = (RangePicker) findViewById(R.id.barsWithThumbs);
+        final RangePicker bars = (RangePicker) findViewById(R.id.barsWithThumbs);
         start = (TextView) findViewById(R.id.textStart);
         end = (TextView) findViewById(R.id.textEnd);
         bars.setOnRangeChangeListener(this);
     }
 
-    @Override public void onRangeChanged(float startRatio, float endRatio) {
-//        start.setText(String.valueOf(startRatio));
-//        end.setText(String.valueOf(endRatio));
-    }
-
-    @Override public void onFinishedMoving(float startRatio, float endRatio) {
+    @Override
+    public void onRangeChanged(float startRatio, float endRatio) {
         start.setText(String.valueOf(startRatio));
         end.setText(String.valueOf(endRatio));
+    }
+
+    @Override
+    public void onFinishedMoving(float startRatio, float endRatio) {
+        // Empty
     }
 }
